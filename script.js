892cccb64f1a2c2041630f01be4cd38cc0d65c3c
zimlow <ziminglow@gmail.com>
2023-04-03 00:16:37 +0800

    complete working game v1
    lacking images and styling

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-const PLAYER_1 = "player1";
-const PLAYER_2 = "player2";
+const FALL_CRITERIA = [
+  [8, 26],
+  [8, 27],
+  [9, 26],
+  [9, 27],
+  [13, 16],
+  [13, 22],
+  [16, 19],
+  [19, 22],
+];
+const B_RIGHT = [0, 1, 2, 3, 4, 6, 9, 10, 12, 16, 18, 19, 22, 24, 25, 26, 27, 28];
+const B_LEFT = [1, 2, 3, 4, 5, 7, 8, 11, 13, 17, 19, 23, 25, 26, 27, 28, 29];
+const T_RIGHT = [6, 7, 8, 9, 10, 12, 13, 16, 18, 22, 24, 27, 28, 30, 31, 32, 33, 34];
+const T_LEFT = [7, 8, 9, 10, 11, 13, 16, 17, 19, 23, 25, 26, 29, 31, 32, 33, 34, 35];
 
-let playerTurn;
+let player1Turn;
 
-const tileElements = document.querySelectorAll("tile");
+const tileElements = document.querySelectorAll(".tile");
 const startPage = document.querySelector(".start-page");
 const gamePage = document.querySelector(".game-page");
 const infoPage = document.querySelector(".info-page");
-const infoButton = document.querySelector(".info");
+const infoButton = document.querySelectorAll(".info");
 const beginButton = document.querySelector(".begin");
 const exit = document.querySelector(".exit-button");
-console.log(infoButton);
+const restartButton = document.querySelectorAll(".restart-button");
+const resultPage = document.querySelector(".result-page");
+const getPeng = document.querySelectorAll(".peng");
+const resultTextMessage = document.querySelector("[result-text]"); //why need []
+const getBoard = document.querySelector(".board");
 
 beginButton.addEventListener("click", beginGame);
-infoButton.addEventListener("click", showInfo);
 
+infoButton.forEach((item) => item.addEventListener("click", showInfo));
+restartButton.forEach((item) => item.addEventListener("click", backToMenu));
+
+//startgame
+//break tile
+//check adjacent tile fell
+//check peng fell
+//swapturns
 function beginGame() {
+  player1Turn = true;
   startPage.classList.remove("show");
   gamePage.classList.add("show");
-  tileElements.addEventListener("click", handleClick);
+
+  tileElements.forEach((element) => {
+    if (element.classList.contains("peng") === false) {
+      element.addEventListener("click", handleClick, { once: true });
+    }
+  });
 }
 
 function showInfo() {
@@ -26,6 +55,82 @@ function showInfo() {
   exit.addEventListener("click", () => infoPage.classList.remove("show"));
 }
 
+function backToMenu() {
+  gamePage.classList.remove("show");
+  startPage.classList.add("show");
+  resultPage.classList.remove("show");
+  tileElements.forEach((element) => {
+    element.classList.remove("fell");
+  });
+}
+
 function handleClick(e) {
-  const tile = e.target;
+  let tile = e.target;
+  tile.classList.add("fell");
+
+  if (checkPengFall()) {
+    console.log("huat ah");
+    endGame();
+  } else {
+    checkAllTiles();
+    swapTurns();
+  }
+}
+
+//stilll has problems. might need to do individual
+function checkAllTiles() {
+  //scan whole board for fallen
+  for (let i = 0; i <= tileElements.length; i++) {
+    if (tileElements[i].classList.contains("fell")) {
+      if (T_LEFT.indexOf(i) >= 0) {
+        if (tileElements[i - 7].classList.contains("fell")) {
+          tileElements[i - 6].classList.add("fell");
+          tileElements[i - 1].classList.add("fell");
+        }
+      }
+
+      if (T_RIGHT.indexOf(i) >= 0) {
+        if (tileElements[i - 5].classList.contains("fell")) {
+          tileElements[i - 6].classList.add("fell");
+          tileElements[i + 1].classList.add("fell");
+        }
+      }
+
+      if (B_LEFT.indexOf(i) >= 0) {
+        if (tileElements[i + 5].classList.contains("fell")) {
+          tileElements[i - 1].classList.add("fell");
+          tileElements[i + 6].classList.add("fell");
+        }
+      }
+
+      if (B_RIGHT.indexOf(i) >= 0) {
+        if (tileElements[i + 7].classList.contains("fell")) {
+          tileElements[i + 1].classList.add("fell");
+          tileElements[i + 6].classList.add("fell");
+        }
+      }
+    }
+    getPeng.forEach((peng) => peng.classList.remove("fell"));
+    return;
+  }
+}
+
+//if inside every combination, at least one tile contains 'fell', there is no more minimum support, fall criteria returns true
+function checkPengFall() {
+  return FALL_CRITERIA.every((combination) => {
+    return combination.some((index) => {
+      return tileElements[index].classList.contains("fell");
+    });
+  });
+}
+
+function swapTurns() {
+  player1Turn = !player1Turn;
+}
+
+function endGame() {
+  resultPage.classList.add("show");
+
+  //whoever's turn cause peng to fall, loses
+  resultTextMessage.innerText = `${player1Turn ? "Player 2" : "Player 1"} wins!`;
 }
